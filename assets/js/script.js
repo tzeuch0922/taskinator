6f87860e29a14cb4eb2c045b539d579e8958cb11
TZeuch <zeuchtony@gmail.com>
2020-08-14 15:57:50 -0500

    can now add tasks using enter key as well as clicking the submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 // Setup element queries
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-function createTaskHandler()
+function createTaskHandler(event)
 {
+    event.preventDefault();
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
