fd7d36ed5fe656ccfdc065941a633ef2619ca18e
TZeuch <zeuchtony@gmail.com>
2020-08-14 16:29:10 -0500

    Add functionality to add task name to tasks successfully

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,20 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 function createTaskHandler(event)
 {
     event.preventDefault();
-    
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // Create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // Create div to hold task info
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
 }
 
